729fa2c3b583e49c5f0e0bae746d4fa75d1f9ad7
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-03-27 17:25:14 -0300

    Add calculator css and operations

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+  return a + b;
+};
+
+function subtract(a, b) {
+  return a - b;
+}
+;
+function multiply(a, b) {
+  return a * b;
+};
+
+function divide(a, b) {
+  return a / b;
+};
+
+function operate(num1, operator, num2) {
+  switch (operator) {
+    case "+": return add(num1, num2);
+    case "-": return subtract(num1, num2);
+    case "x": return multiply(num1, num2);
+    case "÷": return divide(num1, num2);
+  }
+}
