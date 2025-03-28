f2f3304c90a8b10b34fc31cae721c2ecd652248c
Raphaelly Valiengo <102007947+raphaellyv@users.noreply.github.com>
2025-03-28 13:44:47 -0300

    Display keys and result

--- script.js
+++ script.js
@@ -22,3 +22,20 @@ function operate(num1, operator, num2) {
     case "÷": return divide(num1, num2);
   }
 }
+
+const display = document.querySelector("#display");
+let displayedKeys = [];
+
+function displayKey(key) {
+  console.log(key);
+  display.textContent += key;
+  displayedKeys.push(key);
+};
+
+function displayResult() {
+  const num1 = displayedKeys[0];
+  const operator = displayedKeys[1];
+  const num2 = displayedKeys[2];
+
+  display.textContent = operate(num1, operator, num2);
+}
